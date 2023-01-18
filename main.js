0d4a48012b983116c4277c5ddbb9c0f0f06679f7
Pham Thanh Tuan <phamthanhtuan100mc@gmail.com>
2023-01-18 14:56:06 +0900

    test webhook

--- main.js
+++ main.js
@@ -19,6 +19,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/' + 'html/index.html')
 })
 
+app.post('/git', (req, res) => {
+    // If event is "push"
+    if (req.headers['x-github-event'] == "push") {
+        /* Here will be our updating code */
+        // cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+        // cmd.get('./git.sh', (err, data) => {  // Run our script
+        //     if (data) console.log(data);
+        //     if (err) console.log(err);
+        // });
+        // cmd.run('refresh');  // Refresh project
+
+        console.log("> [GIT] Updated with origin/master");
+    }
+
+    return res.sendStatus(200); // Send back OK status
+});
+
 io.on('connection', (socket) => {
     people_count++;
     io.emit('member-add', {
